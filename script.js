3152395f639122b537355bb1f26112af960c2030
Denys Zinchenko <125648199+DenialZlat@users.noreply.github.com>
2025-05-23 17:12:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 window.addEventListener('load', () => {
-    const canvas = document.getElementById('myCanvas');
-    if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
-        const centerX = canvas.width / 2;
-        const centerY = canvas.height / 2;
-
-        function drawSharpEye(color, radiusX, radiusY) {
-            ctx.beginPath();
-            ctx.moveTo(centerX - radiusX, centerY);
-            ctx.quadraticCurveTo(centerX, centerY - radiusY, centerX + radiusX, centerY);
-            ctx.quadraticCurveTo(centerX, centerY + radiusY, centerX - radiusX, centerY);
-            ctx.closePath();
-            ctx.fillStyle = color;
-            ctx.fill();
-        }
-
-        const yellowRGB = 'rgb(255, 200, 0)';
-        drawSharpEye(yellowRGB, 65, 65);
-        drawSharpEye('black', 55, 55);
-        drawSharpEye(yellowRGB, 45, 45);
-        drawSharpEye('black', 35, 35);
-
-        ctx.beginPath();
-        ctx.arc(centerX, centerY, 22.5, 0, Math.PI * 2);
-        ctx.fillStyle = yellowRGB;
-        ctx.fill();
-
-        ctx.fillStyle = 'black';
-        ctx.font = 'bold 38px Sans Serif';
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        ctx.fillText('!', centerX, centerY);
-    }
-});
-
-window.addEventListener('load', () => {
   document.body.classList.add('loaded');
+  const canvas = document.getElementById('myCanvas');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+    const cx = canvas.width / 2, cy = canvas.height / 2;
+    const drawEye = (color, rx, ry) => {
+      ctx.beginPath();
+      ctx.moveTo(cx - rx, cy);
+      ctx.quadraticCurveTo(cx, cy - ry, cx + rx, cy);
+      ctx.quadraticCurveTo(cx, cy + ry, cx - rx, cy);
+      ctx.closePath();
+      ctx.fillStyle = color;
+      ctx.fill();
+    };
+    const yellow = 'rgb(255, 200, 0)';
+    drawEye(yellow, 65, 65);
+    drawEye('black', 55, 55);
+    drawEye(yellow, 45, 45);
+    drawEye('black', 35, 35);
+    ctx.beginPath();
+    ctx.arc(cx, cy, 22.5, 0, Math.PI * 2);
+    ctx.fillStyle = yellow;
+    ctx.fill();
+    ctx.fillStyle = 'black';
+    ctx.font = 'bold 38px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('!', cx, cy);
+  }
 });
